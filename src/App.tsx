ec0da9157c2e26bdb8110aa08ff90f71b24e90df
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-07-08 18:26:40 +0530

    Added speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import Timeline from "./Components/Timeline";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
     return (
         <div className="font-mono">
+            <SpeedInsights />
             <Analytics />
             <Toaster />
             <Home />
