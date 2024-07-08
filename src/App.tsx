875b18b3ac917378fbc576536070909c916caae1
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-07-08 18:23:18 +0530

    Added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { Toaster } from 'react-hot-toast'
-import './App.css'
-import Contact from './Components/Contact'
-import Home from './Components/Home'
-import Projects from './Components/Projects'
-import Skills from './Components/Skills'
-import Timeline from './Components/Timeline'
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
+import Timeline from "./Components/Timeline";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-
-  return (
-    <div className='font-mono'>
-      <Toaster />
-      <Home />
-      <Skills />
-      <Projects />
-      <Timeline />
-      <Contact />
-    </div >
-  )
+    return (
+        <div className="font-mono">
+            <Analytics />
+            <Toaster />
+            <Home />
+            <Skills />
+            <Projects />
+            <Timeline />
+            <Contact />
+        </div>
+    );
 }
 
-export default App
+export default App;
