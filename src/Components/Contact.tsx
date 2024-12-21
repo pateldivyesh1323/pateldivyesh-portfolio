66faa3701c68f4593b2521160c052a3f0bf924cf
Patel Divyesh <pateldivyesh1323@gmail.com>
2024-12-21 17:17:42 +0530

    Changed font size

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
-import Socials from './Socials'
-import toast from 'react-hot-toast';
+import React, { useState } from "react";
+import Socials from "./Socials";
+import toast from "react-hot-toast";
 
 const Contact = (): React.ReactElement => {
-
     const [name, setName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [message, setMessage] = useState<string>("");
@@ -13,54 +12,97 @@ const Contact = (): React.ReactElement => {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         try {
-            await fetch('https://api.web3forms.com/submit', {
+            await fetch("https://api.web3forms.com/submit", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    Accept: "application/json"
+                    Accept: "application/json",
                 },
-                body: JSON.stringify({ name, email, message, access_key: accessKey })
-            })
-            toast.success("I'll be in touch soon")
+                body: JSON.stringify({
+                    name,
+                    email,
+                    message,
+                    access_key: accessKey,
+                }),
+            });
+            toast.success("I'll be in touch soon");
             setName("");
             setEmail("");
             setMessage("");
         } catch (error) {
             toast.error("Failed to send message");
         }
-    }
+    };
 
     return (
-        <div className='contact-component mt-12 mb-8 flex items-center justify-center flex-col gap-8 md:min-h-screen'>
-            <div className='font-bold text-lg underline'>
+        <div className="contact-component mt-12 mb-8 flex items-center justify-center flex-col gap-8 md:min-h-screen">
+            <div className="font-bold underline text-lg">
                 Get in touch with me
             </div>
-            <div className='flex items-center flex-col text-sm md:text-lg'>
-                <form onSubmit={handleSubmit} className='sm:w-[400px] bg-[#4c7273] p-4 md:p-8 mb-8 rounded-2xl flex items-center flex-col'>
-                    <div className='mb-4'>
-                        Send me a message
-                    </div>
-                    <div className='mb-4 w-full'>
+            <div className="flex items-center flex-col text-sm">
+                <form
+                    onSubmit={handleSubmit}
+                    className="sm:w-[360px] bg-[#4c7273] p-4 md:p-8 mb-8 rounded-2xl flex items-center flex-col"
+                >
+                    <div className="mb-4">Send me a message</div>
+                    <div className="mb-4 w-full">
                         <label htmlFor="name">Name: </label>
-                        <input value={name} onChange={(e) => { setName(e.target.value) }} required type="text" name="name" id="name" className='rounded p-2 bg-[#041421] w-full' />
+                        <input
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                            required
+                            type="text"
+                            name="name"
+                            id="name"
+                            className="rounded p-2 bg-[#041421] w-full"
+                        />
                     </div>
-                    <div className='mb-4 w-full'>
+                    <div className="mb-4 w-full">
                         <label htmlFor="email">Email: </label>
-                        <input value={email} onChange={(e) => { setEmail(e.target.value) }} required type="email" name="email" id="email" className='rounded p-2 bg-[#041421] w-full' />
+                        <input
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                            required
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="rounded p-2 bg-[#041421] w-full"
+                        />
                     </div>
-                    <div className='mb-4 w-full'>
+                    <div className="mb-4 w-full">
                         <label htmlFor="message">Message: </label>
-                        <textarea value={message} onChange={(e) => { setMessage(e.target.value) }} required rows={5} name="message" id="message" className='rounded p-2 bg-[#041421] w-full' />
+                        <textarea
+                            value={message}
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                            }}
+                            required
+                            rows={5}
+                            name="message"
+                            id="message"
+                            className="rounded p-2 bg-[#041421] w-full"
+                        />
                     </div>
-                    <div className='w-full'>
-                        <button type='submit' className='p-2 rounded bg-[#041421] float-right'>Send</button>
+                    <div className="w-full">
+                        <button
+                            type="submit"
+                            className="p-2 rounded bg-[#041421] float-right"
+                        >
+                            Send
+                        </button>
                     </div>
                 </form>
-                <a href="mailto:pateldivyesh1323@gmail.com">E-Mail: pateldivyesh1323@gmail.com</a>
+                <a href="mailto:pateldivyesh1323@gmail.com">
+                    E-Mail: pateldivyesh1323@gmail.com
+                </a>
                 <Socials />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
--- src/Components/Timeline.tsx
+++ src/Components/Timeline.tsx
@@ -1,26 +1,23 @@
 export default function Timeline() {
-
     const timeLine = [
         {
             date: "October, 2023",
-            event: "Hacktoberfest"
+            event: "Hacktoberfest",
         },
         {
             date: "November, 2023",
-            event: "MLH November long hackathon"
+            event: "MLH November long hackathon",
         },
         {
             date: "9th February, 2024",
-            event: "Jumble programming by Silver oak university(Runner ups)"
-        }
-    ]
+            event: "Jumble programming by Silver oak university(Runner ups)",
+        },
+    ];
 
     return (
         <div className="flex items-center justify-center flex-col gap-8 mx-8 mt-12 md:min-h-screen">
-            <div className='font-bold text-lg underline'>
-                Timeline
-            </div>
-            <table className="table-auto border border-spacing-2 border-collapse md:text-lg text-xs">
+            <div className="font-bold text-lg underline">Timeline</div>
+            <table className="table-auto border border-spacing-2 border-collapse md:text-base text-xs">
                 <thead>
                     <tr>
                         <th className="border p-4">Date</th>
@@ -34,10 +31,10 @@ export default function Timeline() {
                                 <td className="border p-4">{event.date}</td>
                                 <td className="border p-4">{event.event}</td>
                             </tr>
-                        )
+                        );
                     })}
                 </tbody>
             </table>
         </div>
-    )
+    );
 }
