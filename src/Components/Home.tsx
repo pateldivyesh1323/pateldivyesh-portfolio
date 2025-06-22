c4741bdd157c050c06f74b469709012e9cf47d9f
Divyesh Patel <pateldivyesh1323@gmail.com>
2025-06-22 17:05:28 +0530

    Minor change

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Socials from "./Socials";
 import ThemeToggle from "./ThemeToggle";
 
 const Home = (): React.ReactElement => {
+    const [loaded, setLoaded] = useState(false);
+    const [error, setError] = useState(false);
+
     return (
         <div className="flex items-center flex-col md:flex-row gap-8 my-8 md:my-12 px-4 md:px-6 w-full relative">
             <div className="absolute top-0 right-0 z-10">
                 <ThemeToggle />
             </div>
             <div className="md:ml-0">
+                {(!loaded || error) && (
+                    <div className="w-[120px] h-[120px] rounded-full ring-2 ring-[#FFA500] bg-neutral-300"></div>
+                )}
                 <img
-                    className="w-[120px] rounded-full ring-2 ring-[#FFA500]"
+                    className={`${
+                        loaded &&
+                        !error &&
+                        "w-[120px] rounded-full ring-2 ring-[#FFA500]"
+                    }`}
                     src="./Images/PatelDivyesh-square.jpg"
                     alt="Divyesh Patel"
+                    onLoad={() => setLoaded(true)}
+                    onError={() => setError(true)}
                 />
             </div>
             <div className="text-md max-w-xl text-center md:text-left md:flex-1">
